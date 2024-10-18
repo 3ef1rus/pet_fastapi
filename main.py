b83666d38db2764a8578433cf90b764ee451e88b
3ef1rus <ivanvyal321@gmail.com>
2024-10-18 13:54:47 +0300

    change ignore

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 
-@app.get("/home")
-def get_home():
-    return {"data": "Hello world"}
+@app.get("/name/me")
+async def read_name_me():
+    return {"name": "Ivan"}
+
+
+@app.get("/name/{name}")
+async def read_name(name: str):
+    return {"name": name}
