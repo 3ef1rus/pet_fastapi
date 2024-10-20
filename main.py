ef4044b9bbb1df8f3f48dd96a97675d21962c26e
3ef1rus <ivanvyal321@gmail.com>
2024-10-20 16:31:18 +0300

    use poetr for version control

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
+from typing import Union
+from pydantic import BaseModel, EmailStr
 from fastapi import FastAPI
 
+
 app = FastAPI()
+db_info = [
+    1, 2, 3, 4, 5, 6
+]
 
 
-@app.get("/name/me")
-async def read_name_me():
-    return {"name": "Ivan"}
+class CreateUser(BaseModel):
+    email: EmailStr
 
 
-@app.get("/name/{name}")
-async def read_name(name: str):
-    return {"name": name}
+@app.get("/items/last")
+async def last_item():
+    return {"item1": db_info[-1]}
+
+
+@app.get("/items/{item_id}")
+async def get_itemid(item_id: Union[int, str]):
+    return {"item_id": item_id}
+
+
+@app.get("/hello/")
+async def get_hello_name(name: str = "Ghoste"):
+    return {"message": f"Hello {name}"}
+
+
+@app.post("/users/")
+async def create_user(user: CreateUser):
+    return {
+        "message": "success",
+        "email": user.email,
+    }
+
+
+@app.post("/calc/")
+async def calc(a: Union[int, float], b: Union[int, float], mod: str):
+    if mod == '/':
+        result = a/b
+    if mod == '*':
+        result = a*b
+    if mod == '-':
+        result = a-b
+    if mod == '+':
+        result = a+b
+    return {
+        "a": a,
+        "b": b,
+        "mod": mod,
+        "result": result
+    }
