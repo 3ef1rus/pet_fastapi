5c7f92243b2479ac83410e4a13b77460d29541f0
3ef1rus <ivanvyal321@gmail.com>
2024-10-24 17:23:45 +0300

    install sqlalch and made DB setup

---
+++ core/models/__init__.py
@@ -0,0 +1,9 @@
+from .product import Product
+from .base import Base
+from .db_helper import DatabaseHelper, db_helper
+__all__ = (
+    "Base",
+    "Product",
+    "DatabaseHelper",
+    "db_helper"
+)
